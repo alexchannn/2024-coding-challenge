733546df4a7ebc4a4729580fab0a7e6b97159045
alexchannn <alexyoungreynaldo@gmail.com>
2024-02-02 13:09:51 +0700

    Challenge 4/366

---
+++ 4-smash.js
@@ -0,0 +1,9 @@
+// 2024 Coding Challenge
+// Day 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+function smash(words) {
+   return words.join(" ");
+}
+
+console.log(smash(["Hello", "World", "This", "Is", "Great"]));
