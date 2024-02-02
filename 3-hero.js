8c7a1c4c62b2f98fe867b1e559ef2aceb7cc263d
alexchannn <alexyoungreynaldo@gmail.com>
2024-02-02 10:17:29 +0700

    Update 3/366

--- 3-hero.js
+++ 3-hero.js
@@ -2,12 +2,16 @@
 // Day 3/366
 // https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
 
+// function hero(bullets, dragons) {
+//    if (bullets >= dragons * 2) {
+//       return true;
+//    } else {
+//       return false;
+//    }
+// }
+
 function hero(bullets, dragons) {
-   if (bullets >= dragons * 2) {
-      return true;
-   } else {
-      return false;
-   }
+   return (bullets >= dragons * 2) ? true : false;
 }
 
 console.log(hero(10, 5));
