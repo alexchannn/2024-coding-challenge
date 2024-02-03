5fa6218cef36f7bc3f1195529f22600f993b54a7
alexchannn <alexyoungreynaldo@gmail.com>
2024-02-03 13:56:50 +0700

    Update 4/366

--- 4-smash.js
+++ 4-smash.js
@@ -2,8 +2,19 @@
 // Day 4/366
 // https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
 
+// function smash(words) {
+//    return words.join(" ");
+// }
+
+// const smash = (words) => words.join(" ");
+
 function smash(words) {
-   return words.join(" ");
+   let result = "";
+   for (let i = 0; i < words.length; i++) {
+      result += words[i];
+      if (i != words.length - 1) result += " ";
+   }
+   return result;
 }
 
 console.log(smash(["Hello", "World", "This", "Is", "Great"]));
