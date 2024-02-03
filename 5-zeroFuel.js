c932e7e1a1e40175b685c2b6586be04d1ad980b2
alexchannn <alexyoungreynaldo@gmail.com>
2024-02-03 14:08:56 +0700

    Day 5/366

--- 5-zeroFuel.js
+++ 5-zeroFuel.js
@@ -2,9 +2,15 @@
 // Day 5/366
 // https://codewars.com/kata/5861d28f124b35723e00005e/train/javascript
 
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-   return mpg * fuelLeft >= distanceToPump ? true : false;
-};
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//    return distanceToPump / mpg <= fuelLeft;
+// };
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//    return mpg * fuelLeft >= distanceToPump;
+// };
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
 
 console.log(zeroFuel(50, 25, 2));
 console.log(zeroFuel(100, 50, 1));
